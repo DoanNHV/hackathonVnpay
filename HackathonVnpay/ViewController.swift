4863d51ed6e3a0cc66a8036d9d068d49c5f8ef03
Nguyen Hong Doan <doannh@vnpay.vn>
2023-10-12 22:38:50 +0700

    First Commit

---
+++ HackathonVnpay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackathonVnpay
+//
+//  Created by Nguyen Hong Doan on 12/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
